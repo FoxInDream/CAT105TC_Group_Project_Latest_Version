82b401dfcdcee102ca751db2ea399b183b455a18
FoxInDream <3626578709@qq.com>
2026-05-16 21:10:17 +0800

    2026/05/16_21:10

---
+++ Assets/Script/UI/Instruction.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instruction : MonoBehaviour
+{
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer = 0;
+            Hide();
+
+        }
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
