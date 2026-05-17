e7bd484a74e365a6764e33ed905260f1fd4684a7
OnlineCodeSwimmer <1364677759@qq.com>
2026-05-17 18:01:33 +0800

    1

--- Assets/Script/UI/Instruction.cs
+++ Assets/Script/UI/Instruction.cs
@@ -8,7 +8,7 @@ public class Instruction : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timer = 30;
+        timer = 10;
     }
 
     // Update is called once per frame
